7122bf224524efd6ad4c2d258f246c13686ead65
Mistercage4 <lbg.xqc@outlook.com>
2022-07-24 00:14:55 +0300

    .

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function getComputerChoice(){
     while(randomChoice>2){
         randomChoice = Math.floor(Math.random()*100);
     }
-    console.log(randomChoice);
     computerChoice = choice[randomChoice];
     return computerChoice;
 }
@@ -14,40 +13,64 @@ function getComputerChoice(){
 function playRound(playerselection, computerselection){
     if(playerselection == "rock"){
         if(computerselection == "rock"){
-                document.getElementById('result').innerHTML += "Draw!";
-                document.getElementById('VsMatch').innerHTML += "Rock vs Rock";
+                document.getElementById('result').innerHTML = "Draw!";
+                document.getElementById('VsMatch').innerHTML = "Rock vs Rock";
         }else if(computerselection == "paper"){
-            document.getElementById('result').innerHTML += "Paper beats Rock, Computer wins!";
-            document.getElementById('VsMatch').innerHTML += "Rock vs Paper";
+            document.getElementById('result').innerHTML = "Paper beats Rock, Computer wins!";
+            document.getElementById('VsMatch').innerHTML = "Rock vs Paper";
         }else if(computerselection == "scissors"){
-            document.getElementById('result').innerHTML += "Rock beats Scissors, Player wins!";
-            document.getElementById('VsMatch').innerHTML += "Rock vs Scissors";
+            document.getElementById('result').innerHTML = "Rock beats Scissors, Player wins!";
+            document.getElementById('VsMatch').innerHTML = "Rock vs Scissors";
         }
     }
     if(playerselection == "paper"){
         if(computerselection == "rock"){
-                document.getElementById('result').innerHTML += "Paper beats Rock, Player wins!";
-                document.getElementById('VsMatch').innerHTML += "Paper vs Rock";
+                document.getElementById('result').innerHTML = "Paper beats Rock, Player wins!";
+                document.getElementById('VsMatch').innerHTML = "Paper vs Rock";
         }else if(computerselection == "paper"){
-            document.getElementById('result').innerHTML += "Draw!";
-            document.getElementById('VsMatch').innerHTML += "Paper vs Paper";
+            document.getElementById('result').innerHTML = "Draw!";
+            document.getElementById('VsMatch').innerHTML = "Paper vs Paper";
         }else if(computerselection == "scissors"){
-            document.getElementById('result').innerHTML += "Scissors beats Paper, Computer wins!";
-            document.getElementById('VsMatch').innerHTML += "Paper vs Scissors";
+            document.getElementById('result').innerHTML = "Scissors beats Paper, Computer wins!";
+            document.getElementById('VsMatch').innerHTML = "Paper vs Scissors";
         }
     }
     if(playerselection == "scissors"){
         if(computerselection == "rock"){
-                document.getElementById('result').innerHTML += "Rock beats Scissors, Computer Wins!";
-                document.getElementById('VsMatch').innerHTML += "Scissors vs Rock";
+                document.getElementById('result').innerHTML = "Rock beats Scissors, Computer Wins!";
+                document.getElementById('VsMatch').innerHTML = "Scissors vs Rock";
         }else if(computerselection == "paper"){
-            document.getElementById('result').innerHTML += "Scissors beats Paper, Player wins!";
-            document.getElementById('VsMatch').innerHTML += "Scissors vs Paper";
+            document.getElementById('result').innerHTML = "Scissors beats Paper, Player wins!";
+            document.getElementById('VsMatch').innerHTML = "Scissors vs Paper";
         }else if(computerselection == "scissors"){
-            document.getElementById('result').innerHTML += "Draw!";
-            document.getElementById('VsMatch').innerHTML += "Scissors vs Scissors";
+            document.getElementById('result').innerHTML = "Draw!";
+            document.getElementById('VsMatch').innerHTML = "Scissors vs Scissors";
         }
     }
 }
 
-let playerchoice = prompt("Pick Rock, Paper, or Scissors: ");
+function game(){
+    let playerscore =0, computerscore =0;
+    for(let i=1; i<=5; i++){
+        while(playerscore != 5 && computerscore != 5){
+            let playerchoice = prompt("Pick Rock, Paper, or Scissors: ");
+            playerchoice = playerchoice.toLowerCase();
+            playRound(playerchoice, getComputerChoice());
+            if(document.getElementById('result').innerHTML.includes("Computer wins!")){
+                computerscore++;
+            }else if(document.getElementById('result').innerHTML.includes("Player wins!")){
+                playerscore++;
+            }
+            console.log(playerscore);
+            console.log(computerscore);
+            }
+            
+        }
+        if(playerscore ==5){
+            document.getElementById('finalscore').innerHTML += "Player wins! Score: " + playerscore + " Computer score: " + computerscore;
+        }else if(computerscore==5){
+            document.getElementById('finalscore').innerHTML += "Computer wins! Score: " + computerscore + " Player score: " + playerscore;
+        }
+    }
+
+game();
